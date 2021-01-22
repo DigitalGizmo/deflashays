c1d2b950be2f726ffca3b6450f1bbf239384eb36
Don Button <donpublic@digitalgizmo.com>
2021-01-22 15:43:00 -0500

    first working version

---
+++ scenes/data/Template-svg.js
@@ -0,0 +1,69 @@
+const scene = {
+  "scenewide": {
+    "sceneName": "",
+    // RollTexts are in separate .js file
+  },
+  "tabs": [
+    {
+      "tabName": "Overview",
+      "enabled": true,
+      "hotSpots": [
+      ], // end hotSpots
+    }, // end first tab
+    {
+      "tabName": "English",
+      "enabled": true,
+      "maskSvg": "",
+      "hotSpots": [
+        {
+          "rollName": "",
+          "svg": ""
+        },
+      ], // end hotSpots
+    }, // end layer
+    {
+      "tabName": "French",
+      "enabled": true,
+      "maskSvg": "",
+      "hotSpots": [
+        {
+          "rollName": "",
+          "svg": ""
+        },
+      ], // end hotSpots
+    }, // end layer
+    {
+      "tabName": "KanienKehaka",
+      "enabled": true,
+      "maskSvg": "",
+      "hotSpots": [
+        {
+          "rollName": "",
+          "svg": ""
+        },
+      ], // end hotSpots
+    }, // end layer
+    {
+      "tabName": "Wendat",
+      "enabled": true,
+      "maskSvg": "",
+      "hotSpots": [
+        {
+          "rollName": "",
+          "svg": ""
+        },
+      ], // end hotSpots
+    }, // end layer
+    {
+      "tabName": "Wobanakiak",
+      "enabled": true,
+      "maskSvg": "",
+      "hotSpots": [
+        {
+          "rollName": "",
+          "svg": ""
+        },
+      ], // end hotSpots
+    }, // end layer
+  ], // end tabs
+} // end data
